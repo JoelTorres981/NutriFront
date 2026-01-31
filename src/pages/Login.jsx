bda5e6501e8a2417ab71b238d22c71a5ce2f3e01
JoelTorres981 <joel.torres@epn.edu.ec>
2026-01-31 18:24:35 -0500

    Ingreso sistema

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react';
 import { Link, useNavigate } from "react-router"
-import {useFetch} from '../hooks/useFetch'
+import { useFetch } from '../hooks/useFetch'
 import { ToastContainer } from 'react-toastify'
 import { useForm } from 'react-hook-form'
 import storeAuth from "../context/storeAuth"
 import loginBackground from '/src/assets/images/loginBackground.jpg'
+import { MdVisibility, MdVisibilityOff } from 'react-icons/md'
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate()
     const { register, handleSubmit, formState: { errors } } = useForm()
-    const  fetchDataBackend = useFetch()
+    const fetchDataBackend = useFetch()
     const { setToken, setRol } = storeAuth()
 
-    const loginUser = async(dataForm) => {
-        const url = `${import.meta.env.VITE_BACKEND_URL}/estudiante/login`
-        const response = await fetchDataBackend(url, dataForm,'POST')
+    const loginUser = async (dataForm) => {
+        const url = dataForm.password.includes("ADM")
+            ? `${import.meta.env.VITE_BACKEND_URL}/admin/login`
+            : `${import.meta.env.VITE_BACKEND_URL}/estudiante/login`
+        const response = await fetchDataBackend(url, dataForm, 'POST')
         setToken(response.token)
         setRol(response.rol)
-        if(response){
+        if (response) {
             navigate('/dashboard')
         }
     }
@@ -43,8 +46,8 @@ const Login = () => {
                         {/* Correo electrónico */}
                         <div className="mb-3">
                             <label className="mb-2 block text-sm font-semibold">Correo electrónico</label>
-                            <input type="email" placeholder="Ingresa tu correo" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500" 
-                            {...register("email", { required: "Campo obligatorio" })}
+                            <input type="email" placeholder="Ingresa tu correo" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500"
+                                {...register("email", { required: "Campo obligatorio" })}
                             />
                             {errors.email && <p className="text-red-800 text-xs">{errors.email.message}</p>}
                         </div>
@@ -58,22 +61,14 @@ const Login = () => {
                                     placeholder="Ingresa tu contraseña"
                                     className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500 pr-10"
                                     {...register("password", { required: "Campo obligatorio" })}
-                                    />
-                                    {errors.password && <p className="text-red-800 text-xs">{errors.password.message}</p>}
+                                />
+                                {errors.password && <p className="text-red-800 text-xs">{errors.password.message}</p>}
                                 <button
                                     type="button"
                                     onClick={() => setShowPassword(!showPassword)}
-                                    className="absolute top-2 right-3 text-gray-500 hover:text-gray-700"
+                                    className="absolute top-2 right-3 text-gray-500 hover:text-gray-700 text-xl"
                                 >
-                                    {showPassword ? (
-                                        <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13.875 18.825A9.956 9.956 0 0112 19c-4.418 0-8.165-2.928-9.53-7a10.005 10.005 0 0119.06 0 9.956 9.956 0 01-1.845 3.35M9.9 14.32a3 3 0 114.2-4.2m.5 3.5l3.8 3.8m-3.8-3.8L5.5 5.5" />
-                                        </svg>
-                                    ) : (
-                                        <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0zm-9.95 0a9.96 9.96 0 0119.9 0m-19.9 0a9.96 9.96 0 0119.9 0M3 3l18 18" />
-                                        </svg>
-                                    )}
+                                    {showPassword ? <MdVisibilityOff /> : <MdVisibility />}
                                 </button>
                             </div>
                         </div>
@@ -82,19 +77,6 @@ const Login = () => {
                         <button className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl 
                             hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Iniciar sesión</button>
                     </form>
-
-                    {/* Separador con opción de "O" */}
-                    <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
-                        <hr className="border-gray-400" />
-                        <p className="text-center text-sm">O</p>
-                        <hr className="border-gray-400" />
-                    </div>
-
-                    {/* Botón de inicio de sesión con Google */}
-                    <button className="border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300" style={{ backgroundColor: 'white', color: 'var(--color-primary)', borderColor: 'var(--color-primary)' }}>
-                        <img className="w-5 mr-2" src="https://cdn-icons-png.flaticon.com/512/281/281764.png" alt="Google icon" />
-                        Iniciar sesión con Google
-                    </button>
 
                     {/* Olvidaste tu contraseña */}
                     <div className="mt-5 text-xs border-b-2 py-4">
